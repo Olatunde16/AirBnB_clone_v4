85d38ada6d340a82e238ceda978c9799c589f3ff
derric-d <derric.donehoo@gmail.com>
2020-09-25 21:39:29 +0000

    add working jquery script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,17 @@
-$(document).ready(function () {
-  const amenDict = {};
+$(function () {
+	const amendict = {};
 
-  $('input[type="checkbox"]').click(function () {
-    $(this).each(function () {
-      if (this.checked === true) {
-        amenDict[$(this).data('id')] = $(this).data('name');
-      } else {
-        delete amenDict[($(this).data('id'))];
-      }
-    });
-    if (Object.values(amenDict).lenght > 0) {
-      $('.amenities h4').text(Object.values(amenDict).join(', '));
-    } else {
-      $('.amenities h4').html('&nbsp');
-    }
-  });
+	$('div.amenities li input').change(
+		function () {
+			if ($(this).is(':checked')) {
+				amendict[($(this).attr('data-id'))] = $(this).attr('data-name');
+			} else {
+				delete amendict[($(this).attr('data-id'))]
+		}
+		if (Object.values(amendict).length > 0) {
+			$('.amenities h4').text(Object.values(amendict).join(', '));
+		} else {
+			$('.amenities h4').html('&nbsp');
+		}
+	});
 });
