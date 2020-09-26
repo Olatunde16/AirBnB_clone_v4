614ee4a83322e470bb67be441c2940b0425440ee
Jilroge7 <1672@holbertonschool.com>
2020-09-26 21:55:57 +0000

    edit to 3-hbnb

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,50 +33,26 @@ $(document).ready(function () {
         $('#api_status').removeClass('available');
     }
   });
-	$.ajax({
-		method: "POST",
-		url: 'http://192.168.33.10:5001/api/v1/places_search/',
-		data: JSON.stringify({}),
-		dataType: 'json',
-		contentType: 'application/json',
-		success: function (response) {
-			response.forEach((val, index) => {
-			const foo = $('section.places').add('article').add('div').addClass('title_box').add('h2').html(val.name);
-      $('section.places').after(foo);
-      /*$('section.places article').add('div').addClass('title_box');
-      $('div.title_box').add('h2');
-      $('div.title_box h2').html(val.name);
-      $('div.title_box').add('div').addClass('price_by_night');
-      $('div.price_by_night').add(val.price_by_night);
-      $('section.places').add('div').addClass('information');
-      $('div.information').add('div').addClass('max_guest');
-      $('div.max_guest').html(val.max_guest);
-      $('div.information').add('div').addClass('number_rooms');
-      $('div.number_rooms').add(val.number_rooms);
-      $('div.information').add('div').addClass('number_bathrooms');
-      $('div.number_bathrooms').html(val.number_bathrooms);
-      $('section.places').add('div').addClass('description');
-      $('div.description').html(val.description);*/
-    });
-		}
-		});
-/*  $.post('http://192.168.33.10:5001/api/v1/places_search/', function(data) {
-		console.log("inside get");i*/
-/*    data.forEach((val, index) => {
-      $('section.places').add('article');
-      $('section.places article').add('div').addClass('title_box');
-      $('div.title_box').add('h2');
-      $('div.title_box h2').add(val.name);
-      $('div.title_box').add('div').addClass('price_by_night');
-      $('div.price_by_night').add(val.price_by_night);
-      $('section.places').add('div').addClass('information');
-      $('div.information').add('div').addClass('max_guest');
-      $('div.max_guest').add(val.max_guest);
-      $('div.information').add('div').addClass('number_rooms');
-      $('div.number_rooms').add(val.number_rooms);
-      $('div.information').add('div').addClass('number_bathrooms');
-      $('div.number_bathrooms').add(val.number_bathrooms);
-      $('section.places').add('div').addClass('description');
-      $('div.description').add(val.description);
-    });*/
+  $.ajax({
+    method: "POST",
+    url: 'http://192.168.33.10:5001/api/v1/places_search/',
+    data: JSON.stringify({}),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (response) {
+      const title_box = "article><div class='title_box'><h2></h2><div class='price_by_night'</div></div>";
+      const info = "<div class='information'><div class='max_guest'</div><div class='number_rooms'></div>";
+      const num_bath = "<div class='number_bathrooms'</div></div><div class='description'</div></article>";
+      const article = title_box + info + num_bath;
+      response.forEach((val, index) => {
+        $('section.places').append(article);
+        $('.title_box h2').last().html(val.name);
+        $('.price_by_night').last().html(val.price_by_night);
+        $('.max_guest').last().html(val.max_guest);
+        $('.number_rooms').last().html(val.number_rooms);
+        $('.number_bathrooms').last().html(val.number_bathrooms);
+        $('.description').last().html(val.description);
+      });
+    }
+  });
 });
