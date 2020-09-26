511f4952b209c03ea99adcbf799b9bf183d1714a
Viet Tran <vietnamtran@gmail.com>
2020-09-26 17:31:07 +0000

    Updated code for tasks 2 and 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,26 +1,36 @@
 #!/usr/bin/node
 $(document).ready(function () {
-  const dict = {};
+	const dict = {};
   $('INPUT:checkbox').change(function () {
-		const k = $(this).attr('data-id');
+    const k = $(this).attr('data-id');
     const v = $(this).attr('data-name');
     if ((this).checked) {
-			if (!dict[k]) {
-				dict[k] = v;
-			}
-			$('.amenitites h4').append(dict[k]);
-    } else {
-			delete dict[k];
-			$('.amenitites h4').remove(dict[k]);
+      if (!dict[k]) {
+        dict[k] = v;
+      }
+    } else if (!(this).checked) {
+      delete dict[k];
     }
-    console.log(dict);
-    $('.amenitites h4').append(dict[k]);
+    $('.amenities h4').empty();
+    const vals = Object.values(dict);
+    const length = vals.length;
+    vals.forEach((val, index) => {
+      $('.amenities h4').append(val);
+      console.log(dict);
+      if (index < length - 1) {
+        $('.amenities h4').append(', ');
+      }
+    });
+    if (length === 0) {
+      console.log('dict is empty');
+      $('.amenities h4').append('&nbsp;');
+    }
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function(data, textstatus) {
-    if (textstatus === 'success') {
+	$.getJSON('http://192.168.33.10:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
         $('#api_status').addClass('available');
     } else {
         $('#api_status').removeClass('available');
     }
+  });
 });
-
