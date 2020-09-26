36b3542139b938a2cd5272d9f021055efa96b7f1
Jilroge7 <1672@holbertonschool.com>
2020-09-26 18:39:31 +0000

    adding 3-hbnb

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,54 @@
+#!/usr/bin/node
+$(document).ready(function () {
+	const dict = {};
+  $('INPUT:checkbox').change(function () {
+    const k = $(this).attr('data-id');
+    const v = $(this).attr('data-name');
+    if ((this).checked) {
+      if (!dict[k]) {
+        dict[k] = v;
+      }
+    } else if (!(this).checked) {
+      delete dict[k];
+    }
+    $('.amenities h4').empty();
+    const vals = Object.values(dict);
+    const length = vals.length;
+    vals.forEach((val, index) => {
+      $('.amenities h4').append(val);
+      console.log(dict);
+      if (index < length - 1) {
+        $('.amenities h4').append(', ');
+      }
+    });
+    if (length === 0) {
+      console.log('dict is empty');
+      $('.amenities h4').append('&nbsp;');
+    }
+  });
+  $.getJSON('http://192.168.33.10:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+    } else {
+        $('#api_status').removeClass('available');
+    }
+  });
+  $.getJSON('http://192.168.33.10:5001/api/v1/places_search', function(data) {
+    data.forEach((val, index) => {
+      $('section.places').add('article');
+      $('section.places article').add('div').addClass('title_box');
+      $('div.title_box').add('h2');
+      $('div.title_box h2').add(val.name);
+      $('div.title_box').add('div').addClass('price_by_night');
+      $('div.price_by_night').add(val.price_by_night);
+      $('section.places').add('div').addClass('information');
+      $('div.information').add('div').addClass('max_guest');
+      $('div.max_guest').add(val.max_guest);
+      $('div.information').add('div').addClass('number_rooms');
+      $('div.number_rooms').add(val.number_rooms);
+      $('div.information').add('div').addClass('number_bathrooms');
+      $('div.number_bathrooms').add(val.number_bathrooms);
+      $('section.places').add('div').addClass('description');
+      $('div.description').add(val.description);
+    });
+});
