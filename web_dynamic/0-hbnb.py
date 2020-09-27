178f747041ffe629a993c948835d8334051632ab
davehh1211 <davehh1211@gmail.com>
2020-09-27 01:10:31 +0000

    fixes in files due merge

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -41,7 +41,7 @@ def hbnb():
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
