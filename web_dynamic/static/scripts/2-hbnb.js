4eff0b789d520668ae97c40c9d868e69802e2bea
Ang3l1t0 <1546@holbertonschool.com>
2020-09-27 16:07:15 -0500

    Task 3 files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,3 +12,15 @@ function init () {
     $('.amenities h4').text(names.sort().join(', '));
   });
 }
+
+// Check API status and update status in the header
+
+const url = 'http://0.0.0.0:5001/api/v1/status/';
+$get(url, function (data, status) {
+  console.log(data);
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
