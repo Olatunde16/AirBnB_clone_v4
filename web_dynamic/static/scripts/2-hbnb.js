11539803dc6bf07effdd27acd2626b0d6e99d0e1
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-27 21:04:03 +0000

    task 3 fixed api file, header api status to change and added quotes on amenities of 2 html

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,8 +21,8 @@ $(document).ready(function () {
       }
     }
   });
-  $.get("http://0.0.0.0:5001/api/v1/status/", function(data){
-    if (data === 'OK'){
+  $.getJSON("http://localhost:5001/api/v1/status/", function(data){
+    if (data.status === 'OK'){
       $('div#api_status').addClass('available');
     }else{
       $('div#api_status').removeClass('available');
