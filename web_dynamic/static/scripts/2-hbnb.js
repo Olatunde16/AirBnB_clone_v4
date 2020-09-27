923e89b05a8907b22c924192178df5f8779d09a9
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-27 04:23:07 +0000

    2-hbnb js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,4 +21,11 @@ $(document).ready(function () {
       }
     }
   });
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data){
+    if (data === 'OK'){
+      $('div#api_status').addClass('available');
+    }else{
+      $('div#api_status').removeClass('available');
+    }
+  });
 });
