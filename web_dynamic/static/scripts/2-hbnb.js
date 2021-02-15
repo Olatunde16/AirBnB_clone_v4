78b070822f22ebfaa07a59064206978d875d621d
blsoko <exanymejose1@hotmail.com>
2021-02-15 15:53:00 -0500

    Create javascript and display from DOM

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,4 +15,12 @@ $(document).ready(function () {
       $('.amenities h4').text(Object.values(amenitiesDict).join(', '));
     }
   });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
 });
