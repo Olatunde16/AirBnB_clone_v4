b2b65e490ac71aa7fb78eb6a00ce7e785d0bac7f
1819@holbertonschool.com <1819@holbertonschool.com>
2021-02-16 10:42:40 -0600

    adding uuid to places

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,6 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -33,6 +34,8 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+
+    cache_id = uuid.uuid4();
 
     return render_template('0-hbnb.html',
                            states=st_ct,
