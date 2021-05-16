be7be1ccd1c390e6896759d36c5abccfbbd5b702
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-16 11:10:01 +0000

    Keeps track of the original context of <h4>, if every box is unchecked it set the original text again

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   const checkedAmenities = {};
   const checkboxes = $('input');
+  const initial_text = $('DIV.amenities h4').text();
   for (const box of checkboxes) {
     box.addEventListener('change', function () {
       if (box.checked) {
@@ -9,6 +10,9 @@ $(document).ready(function () {
         delete checkedAmenities[$(box).data('id')];
       }
       const checkedList = Object.values(checkedAmenities);
+      if (checkedList.length < 1 ) {
+        checkedList.push(initial_text);
+      }
       $('DIV.amenities h4').text(checkedList.join(', '));
     });
   }
