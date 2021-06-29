8508f0dc55a5e5d92c3da8140111453cc7b07398
rkbrian <rkbriankong@gmail.com>
2021-06-29 22:29:06 +0000

    fix js and html for server run up

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,19 @@
-#!/usr/bin/node
-const amdict = {};
-// Attach a change event handler to the checkboxes.
-$("input").change(function() {
-    $("input").each(function() {
-        if (klikcmdr.checked=True) {
-            amdict.append($("klikcmdr").data);
-        } else {
-            amdict.pop($("klikcmdr").data);
+$(document).ready(function () {
+    const amdict = {};
+
+    // Attach a change event handler to the checkboxes.
+    $("input").click(function() {
+	$(":input").each(function() {
+            if (this.checked===true) {
+		amdict[$(this).data('name')] = $(this).data('id');
+            } else {
+		delete amdict[$(this).data('name')];
+	    }
+	});
+	const arr = [];
+	for (const key in amdict) {
+	    arr.push(key);
 	}
+	$(".amenities h4").html(arr.join(', '));
     });
 });
