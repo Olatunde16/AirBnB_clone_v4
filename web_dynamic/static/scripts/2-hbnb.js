be63357bec5d469b0829fe0d211a36e9fd1674de
Lonzo Rust <rustlonzo@gmail.com>
2021-06-29 23:06:18 +0000

    Adding corrected files for task3. Fully working.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,8 +1,25 @@
+// script for checkboxes and API listening
 $(document).ready(function () {
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  let tmp_dic = {};
+  $('input').click(function () {
+    $(':input').each(function () {
+      if (this.checked === true) {
+        tmp_dic[$(this).data('name')] = $(this).data('id');
+      } else {
+        delete tmp_dic[$(this).data('name')];
+      }
+    });
+    let amen_lst = [];
+    for (const k in tmp_dic) {
+      amen_lst.push(k);
+    }
+  $('DIV.amenities h4').html(amen_lst.join(', '));
+  });
+    $.get('http://810428cc5457.b594c0bb.hbtn-cod.io:5001/api/v1/status/', function (data) {
 	if (data.status === 'OK') {
 	    $('DIV#api_status').addClass('available');
 	} else {
 	    $('DIV#api_status').removeClass('available');
 	}
+    });
 });
