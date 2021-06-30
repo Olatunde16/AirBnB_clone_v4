96c3478cd526307f61a1ec4d96619c37e1b6a011
Rachel Lewis <RLewis11769@gmail.com>
2021-06-30 04:51:22 +0000

    Semistandard style correction

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-    let checkboxes = $("input[type=checkbox][data-name]")
-    let enabled = [];
-    let names = [];
+  const checkboxes = $('input[type=checkbox][data-name]');
+  let enabled = [];
+  let names = [];
 
-    checkboxes.change(function() {
-	enabled = checkboxes
-	    .filter(":checked")
-	    .map(function() {
-		return this.dataset.id
-	    })
-	    .get()
-	names = checkboxes
-            .filter(":checked")
-            .map(function() {
-		    return this.nextSibling.wholeText
-            })
-            .get()
-	console.log(enabled);
-	text = ""
-	for (name in names) {
-	    text += names[name]
-	    if (name < names.length - 1) {
-		text += ', '
-	    }
-	}
-	$('.amenities h4').html(text)
-    });
+  checkboxes.change(function () {
+    enabled = checkboxes
+      .filter(':checked')
+      .map(function () {
+        return this.dataset.id;
+      })
+      .get();
+    names = checkboxes
+      .filter(':checked')
+      .map(function () {
+        return this.nextSibling.wholeText;
+      })
+      .get();
+    console.log(enabled);
+    let text = '';
+    for (const nam in names) {
+      text += names[nam];
+      if (nam < names.length - 1) {
+        text += ', ';
+      }
+    }
+    $('.amenities h4').html(text);
+  });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,39 +1,38 @@
 $(document).ready(function () {
-    let checkboxes = $("input[type=checkbox][data-name]")
-    let enabled = [];
-    let names = [];
+  const checkboxes = $('input[type=checkbox][data-name]');
+  let enabled = [];
+  let names = [];
 
-    checkboxes.change(function() {
-	enabled = checkboxes
-	    .filter(":checked")
-	    .map(function() {
-		return this.dataset.id
-	    })
-	    .get()
-	names = checkboxes
-            .filter(":checked")
-            .map(function() {
-		    return this.nextSibling.wholeText
-            })
-            .get()
-	console.log(enabled);
-	text = ""
-	for (name in names) {
-	    text += names[name]
-	    if (name < names.length - 1) {
-		text += ', '
-	    }
-	}
-	$('.amenities h4').html(text)
-    });
+  checkboxes.change(function () {
+    enabled = checkboxes
+      .filter(':checked')
+      .map(function () {
+        return this.dataset.id;
+      })
+      .get();
+    names = checkboxes
+      .filter(':checked')
+      .map(function () {
+        return this.nextSibling.wholeText;
+      })
+      .get();
+    let text = '';
+    for (const nam in names) {
+      text += names[nam];
+      if (nam < names.length - 1) {
+        text += ', ';
+      }
+    }
+    $('.amenities h4').html(text);
+  });
 });
 
-$(document).ready(function() {
-    $.getJSON("http://0.0.0.0:5001/api/v1/status/", function(data) {
-	if (data.status === "OK") {
-	    $('#api_status').addClass('available');
-	} else {
-	    $('#api_status').removeClass('available');
-	}
-    });
+$(document).ready(function () {
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
