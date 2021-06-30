b51af15262fef925bed200386957a31b371d3bcf
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-30 04:43:16 +0000

    adding task 4 web dynamic project.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
             '<article>' +
             '<div class="title_box">' +
             '<h2>' + place.name + '</h2>' +
-            '<div class="price_by_night">' + place.price_by_night + '</div>' +
+            '<div class="price_by_night">$' + place.price_by_night + '</div>' +
             '</div>' +
             '<div class="information">' +
             '<div class="max_guest">' + place.max_guest +
