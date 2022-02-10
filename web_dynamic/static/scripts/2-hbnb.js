4f9acd6fb513b9610682f05a7129557ea2479708
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-02-10 10:44:37 -0500

    [UPDATE] Updated apiAvailable function for task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,29 +1,27 @@
 const $ = window.jQuery;
 const listAmenities = {};
 window.onload = function () {
-  select_amenities();
-  api_availabe();
+  selectAmenities();
+  apiAvailabe();
 };
 
-function select_amenities() {
-    $('div.amenities input[type=checkbox]').change(function () {
-        const id = $(this).attr('data-id');
-        if ($(this).is(':checked')) {
-          listAmenities[id] = $(this).attr('data-name');
-        } else {
-          delete listAmenities[id];
-        }
-        $('.amenities h4').text(Object.values(listAmenities).join(', '));
-      });
-    
+function selectAmenities () {
+  $('div.amenities input[type=checkbox]').change(function () {
+    const id = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      listAmenities[id] = $(this).attr('data-name');
+    } else {
+      delete listAmenities[id];
+    }
+    $('.amenities h4').text(Object.values(listAmenities).join(', '));
+  });
 }
 
-async function api_availabe() {
-  try{
-  const data = await fetch("http://0.0.0.0:5001/api/v1/status/")
-  if (!data.ok) {
-    
-  }
-  
-  } catch{console.log("No connection")}
-};
+async function apiAvailabe () {
+  $.get('http://172.24.191.151:5001/api/v1/status/', function (data, status) {
+    console.log(data);
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
+  });
+}
