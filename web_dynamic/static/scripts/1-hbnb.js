d83750926823bbdec1e879d30316ab3b5beb1068
luffy <jhonsmith981@hotmail.com>
2022-02-11 01:47:26 -0500

    Jquery to amenities Airbnb

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(function(){
+  const AmenitiesChecked = {}
+  $(document).on('change', "input[type='checkbox']", function (){
+    if (this.checked){
+      AmenitiesChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete AmenitiesChecked[$(this).data('id')];
+    }
+    const Objs = Object.values(AmenitiesChecked);
+    console.log(Object.values(AmenitiesChecked));
+    if (Objs){
+      $('.amenities > h4').text(Object.values(AmenitiesChecked).join(', '));
+    } else {
+      $('.amenities > h4').html('&nbsp;');
+    }
+  });
+});
