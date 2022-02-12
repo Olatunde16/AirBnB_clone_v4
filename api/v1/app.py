af4447d361b027abccbc096a1655fd15b8c53903
luffy <jhonsmith981@hotmail.com>
2022-02-11 22:07:34 -0500

    API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,11 @@ from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -29,6 +30,7 @@ def not_found(error):
         description: a resource was not found
     """
     return make_response(jsonify({'error': "Not found"}), 404)
+
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
