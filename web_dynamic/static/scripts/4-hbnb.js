7533cd9017059f54f1f17f4f59baff1d3ba8a646
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-25 21:41:03 +0300

    task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,66 @@
+$( document ).ready(function () {
+
+  /*****************************************************
+    display checkboxes clicked
+   *****************************************************/
+  let ls_amen = {};
+  $('input[type=checkbox]').change (function () {
+    if ($(this).is(':checked')) {
+      ls_amen[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete ls_amen[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(ls_amen).join(', '));
+  });
+
+  /*******************************************************
+    display red circle on top right if status ok
+   *******************************************************/
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+	$('#api_status').addClass('available');
+      } else {
+	$('#api_status').removeClass('available');
+      }
+    }
+  });
+
+  /*******************************************************
+    populate Places from frontend, instead of backend
+   *******************************************************/
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({}),
+      contentType: 'application/json',
+      success: function (data) {
+	for (let i = 0; i < data.length; i++) {
+	  $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
+	}
+      }
+    });
+
+  /*******************************************************
+    populate Places from frontend, instead of backend;
+    filter places displayed based on amenity checkboxed list
+   *******************************************************/
+  $('button').click(function () {
+    $('article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({'amenities': Object.keys(ls_amen)}),
+      contentType: 'application/json',
+      success: function (data) {
+	for (let i = 0; i < data.length; i++) {
+	  $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
+	}
+      }
+    });
+  });
+
+});
