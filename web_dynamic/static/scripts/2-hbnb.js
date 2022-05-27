e47820bf105045fa76cd25b774f865fc275cc802
Falendrac <leger_franck@hotmail.fr>
2022-05-27 15:08:39 +0200

    Add all changes for the task 3, web_dynamic.

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+/*
+JavaScript script
+*/
+let amenitiesChecked = []
+document.addEventListener('DOMContentLoaded', (event) => {
+    window.$('input:checkbox').change(function() {
+        cbName = $(this).attr("data-name")
+        if(window.$(this).is(':checked')) {
+            amenitiesChecked.push(cbName)
+        } else {
+            delete amenitiesChecked.pop(cbName)
+        }
+        $('.amenities h4').html(amenitiesChecked.join(", "));
+    });
+
+    $.get("http://0.0.0.0:5001/api/v1/status/", function(data, testStatus) {
+        if (data['status'] === "OK") {
+            $('div#api_status').addClass('available')
+        } else {
+            $('div#api_status').remove('available')
+        }
+    });
+
+});
+
+
