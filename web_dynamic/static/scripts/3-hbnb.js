1f482fb12122b24373fddaaea5c50794df59af06
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-05-28 12:23:41 +0000

    update task 4 and add places loading from API

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,29 +1,54 @@
+#!/usr/bin/node
 /*
-JavaScript script
+JavaScript script that load hbnb page
 */
-let amenitiesChecked = []
+const amenitiesChecked = [];
 document.addEventListener('DOMContentLoaded', (event) => {
-    window.$('input:checkbox').change(function() {
-        cbName = $(this).attr("data-name")
-        if(window.$(this).is(':checked')) {
-            amenitiesChecked.push(cbName)
-        } else {
-            delete amenitiesChecked.pop(cbName)
-        }
-        $('.amenities h4').html(amenitiesChecked.join(", "));
-    });
+  window.$('input:checkbox').change(function () {
+    const cbName = $(this).attr('data-name');
+    if (window.$(this).is(':checked')) {
+      amenitiesChecked.push(cbName);
+    } else {
+      delete amenitiesChecked.pop(cbName);
+    }
+    $('.amenities h4').html(amenitiesChecked.join(', '));
+  });
 
-    $.get("http://0.0.0.0:5001/api/v1/status/", function(data, testStatus) {
-        if (data['status'] === "OK") {
-            $('div#api_status').addClass('available')
-        } else {
-            $('div#api_status').remove('available')
-        }
-    });
+  $.get('http://127.0.0.1:5002/api/v1/status/', function (data, testStatus) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').remove('available');
+    }
+  });
 
-    $.post("http://0.0.0.0:5001/api/v1/places_search/", {
-        "Content-Type": "application/json"
-    }, function(result) {
-        alert(result)
-    });
+  $.ajax({
+    type: 'POST',
+    url: 'http://127.0.0.1:5002/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (response) {
+      for (const [, value] of Object.entries(response)) {
+        const bathrooms = (`${value.number_bathrooms}` > 1 ? 'Bathrooms' : 'Bathroom');
+        const guests = (`${value.max_guest}` > 1 ? 'Guests' : 'Guest');
+        const rooms = (`${value.number_rooms}` > 1 ? 'Rooms' : 'Room');
+        const render = `<article>
+                <div class="title_box">
+                    <h2>${value.name}</h2>
+                    <div class="price_by_night">$${value.price_by_night}</div>
+                </div>
+                <div class="information">
+                    <div class="max_guest">${value.max_guest} ${guests}</div>
+                        <div class="number_rooms">${value.number_rooms} ${rooms}</div>
+                        <div class="number_bathrooms">${value.number_bathrooms} ${bathrooms}</div>
+                </div>
+                <div class="description">
+                    ${value.description}
+                    </div>
+                </article>`;
+        $('.places').append(render);
+      }
+    }
+  });
 });
