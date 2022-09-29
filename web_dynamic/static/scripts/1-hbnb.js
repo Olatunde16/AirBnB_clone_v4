00f78171e9144c42d9811dac5635a1b8b20bc8b7
Chiara Caprasi <caprasichiara@gmail.com>
2022-09-29 17:52:50 +1000

    add 1-hbnb.js for task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function () {
+  let checkedAmenities = {};
+  $("input[type=checkbox]").change(function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data("id")] = $(this).data("name");
+    } else {
+      delete checkedAmenities[$(this).data("id")];
+    }
+    const amenities = Object.keys(checkedAmenities);
+    $("div.amenities > h4").text(amenities.join(", "));
+  });
+});
