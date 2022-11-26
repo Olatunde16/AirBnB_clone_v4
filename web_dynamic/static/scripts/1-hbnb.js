37a2621219841f21a8c927cf67f3fd77c4d57a13
UnfazedAy <ayomidesoniyi@gmail.com>
2022-11-26 11:10:19 +0100

    Corrected an indentation

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,10 +2,10 @@ $( document ).ready(() => {
   let amenities = {};
   $('INPUT[type="checkbox"]').change(() => {
     if ($(this).is(':checked')) {
-        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
       delete amenities[$(this).attr('data-id')];
     }
     $('.amenities H4').text(Object.values(amenities).join(', '));
   })
-})
+});
