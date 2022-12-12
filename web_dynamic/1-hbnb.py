7bcf85e0ae7264f96298edd714e341f7837a6f9d
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-12-11 20:29:40 -0600

    Edit: Initial commits for task 1 of v4 - updated 100-hbnb.py/html to 0-hbnb.py/html and replaced route in 0-hbnb.py to /0-hbnb/

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+# from os import environ
+import uuid
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
+# from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
-from flask import Flask, render_template
-import uuid
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -43,5 +44,4 @@ def hbnb():
 
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
