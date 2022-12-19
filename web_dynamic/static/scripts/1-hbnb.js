d777e3977d9b22c4b1ec646f5353f7f99b1908ec
ikerikks <ikerikks@gmail.com>
2022-12-19 19:10:36 +0100

    task2: script js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+
+const dict = {};
+
+$(document).ready(function() {
+    $('input[type=checkbox]').change(function () {
+        if (this.checked) {
+          dict[$(this).attr('data-id')] = $(this).attr('data-name');
+        } else {
+          delete dict[$(this).attr('data-id')];
+        }
+    });
+});
+
+
+
+
+
