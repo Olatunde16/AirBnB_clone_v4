3beba79ca2fad5b159fbec562c0bb19f863f7f91
ikerikks <ikerikks@gmail.com>
2022-12-19 19:20:44 +0100

    adding assigment in statements

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -5,8 +5,10 @@ $(document).ready(function () {
   $('input[type=checkbox]').change(function () {
     if (this.checked) {
       dict[$(this).attr('data-id')] = $(this).attr('data-name');
+      $('.amenities h4').text($(this).attr('data-name'));
     } else {
       delete dict[$(this).attr('data-id')];
+      $('.amenities h4').text('');
     }
   });
 });
