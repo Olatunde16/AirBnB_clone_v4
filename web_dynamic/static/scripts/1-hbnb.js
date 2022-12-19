00e538e9f77624737cee93cee42d99ca02acf8da
ikerikks <ikerikks@gmail.com>
2022-12-19 19:12:21 +0100

    correction of js style

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,12 @@
 
 const dict = {};
 
-$(document).ready(function() {
-    $('input[type=checkbox]').change(function () {
-        if (this.checked) {
-          dict[$(this).attr('data-id')] = $(this).attr('data-name');
-        } else {
-          delete dict[$(this).attr('data-id')];
-        }
-    });
+$(document).ready(function () {
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      dict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete dict[$(this).attr('data-id')];
+    }
+  });
 });
-
-
-
-
-
