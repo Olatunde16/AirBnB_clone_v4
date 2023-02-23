726c8b0bdf44984374966c05fefaa3063fc1aaa5
Peter-Brian <studygroup226@gmail.com>
2023-02-23 09:17:29 +0000

    adding

---
+++ web_dynamic/4-hbnb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Flask app to generate complete html page containing location/amenity
+dropdown menus and rental listings"""
+from flask import Flask, render_template
+from models import storage
+from uuid import uuid4
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/4-hbnb')
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    return render_template('4-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid4())
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
