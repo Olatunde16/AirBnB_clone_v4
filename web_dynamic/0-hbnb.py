98a997ed6c4231fe7b21324b7db13123dc2ce360
JasonBeagle <skipskipperson385@gmail.com>
2023-04-16 09:44:06 -0700

    adding README, created template folder, moved html, made slight change to .py as well

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,6 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+from uuid import uuid4
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -18,12 +19,14 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
+    # "Task 1"
+    cache_id = str(uuid4())
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -34,10 +37,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    "task 1"
-    cache_id = str(uuid.uuid4())
-
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places)
---
+++ web_dynamic/README.md
@@ -0,0 +1,17 @@
+# Proj 0x05. AirBnB clone - Web dynamic
+
+## Learning Objectives
+
+    How cool it is to request your own API
+    How to modify an HTML element style
+    How to get and update an HTML element content
+    How to modify the DOM
+    How to make a GET request with JQuery Ajax
+    How to make a POST request with JQuery Ajax
+    How to listen/bind to DOM events
+    How to listen/bind to user events
+
+
+
+Jacob Marshall - [Github](https://github.com/JacobM311)
+Jason Beagle - [Github](https://github.com/JasonBeagle)
