f24f7f05b9a778d85d73c4a38c0ffd19bfed0ed1
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-25 16:06:18 -0500

    script updated

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,58 @@
+$('document').ready(function () {
+    let amenities = {};
+    $('INPUT[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      if (Object.values(amenities).length === 0) {
+        $('.amenities H4').html('&nbsp;');
+      } else {
+        $('.amenities H4').text(Object.values(amenities).join(', '));
+      }
+    });
+
+    $.get("http://localhost:5001/api/v1/status", (data) => {
+        if (data.status == 'OK') {
+            $('div#api_status').addClass('available');
+        }
+    })
+
+    $.ajax({
+      url: api + ':5001/api/v1/places_search/',
+      type: 'POST',
+      data: '{}',
+      contentType: 'application/json',
+      dataType: 'json',
+      success: appendPlaces
+    });
+})
+
+function appendPlaces (data) {
+  $('SECTION.places').empty();
+  $('SECTION.places').append(data.map(place => {
+    return `<ARTICLE>
+              <DIV class="title">
+                <H2>${place.name}</H2>
+                  <DIV class="price_by_night">
+                    ${place.price_by_night}
+                  </DIV>
+                </DIV>
+                <DIV class="information">
+                  <DIV class="max_guest">
+                    ${place.max_guest} Guests
+                  </DIV>
+                  <DIV class="number_rooms">
+                    ${place.number_rooms} Bedrooms
+                  </DIV>
+                  <DIV class="number_bathrooms">
+                    ${place.number_bathrooms} Bathrooms
+                  </DIV>
+                </DIV>
+                <DIV class="description">
+                  ${place.description}
+                </DIV>
+              </ARTICLE>`;
+  }));
+}
