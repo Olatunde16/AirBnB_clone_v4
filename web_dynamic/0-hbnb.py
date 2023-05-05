44c574eb84ab49a1b20ecab4ce9cf867b7a5be25
efezino idisi <enduranceidisi13@gmail.com>
2023-05-05 17:21:19 +0100

    fix: pycodestyle error

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -39,8 +39,7 @@ def hbnb():
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid4()
-    )
+                           cache_id=uuid4())
 
 
 if __name__ == "__main__":
