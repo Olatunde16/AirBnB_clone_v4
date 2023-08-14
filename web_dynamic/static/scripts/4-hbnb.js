65f7592e410747e69b9929ffd9a04c3877580360
Jabulani-N <jabu.94@gmail.com>
2023-08-14 16:40:17 -0700

    task5:added empty function on buttonclick

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -51,5 +51,9 @@ $(document).ready(() => {
     });
     $('#button').click(function () {
       // perform a function
+
+      // new POST request to places_search should be made
+      // with the list of Amenities checked
+
     });
   });
