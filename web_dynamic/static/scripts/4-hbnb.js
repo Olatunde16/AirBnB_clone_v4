df2066655bd63491675316450070b97efdb7b951
Ben Harper <benjaminitonio@gmail.com>
2023-08-15 14:21:07 -0500

    working on 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -11,6 +11,11 @@ $(document).ready(function () {
         }
         $('div.amenities h4').text(listAmenities.join(', '));
     });
+
+    $('button:eq(0)').click(function() {
+      filterPlaces('http://127.0.0.1:5001/api/v1/places_search', listAmenities);
+    });
+
     //gets the status of the api server
     requestAPI('http://127.0.0.1:5001/api/v1/status/');
     //retrieves all places
@@ -37,6 +42,56 @@ function places (url) {
     success: function(data) {
       for (const place of data) {
         $.get('http://127.0.0.1:5001/api/v1/users/' + place.user_id, function (userData) {
+          let guestsPlural = '';
+          if (place.max_guest != 1) {
+            let guestsPlural ='s';
+          }
+          let roomsPlural = ''
+          if (place.number_rooms != 1) {
+            let roomsPlural ='s';
+          }
+          let bathroomPlural = ''
+          if (place.number_bathrooms != 1) {
+            let bathroomPlural ='s';
+          }
+          let html = `<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${guestsPlural}</div>
+                  <div class="number_rooms">${place.number_rooms } Bedroom${roomsPlural}</div>
+                  <div class="number_bathrooms">${place.number_bathrooms} Bathroom${bathroomPlural}</div>
+          </div>
+          <div class="user">
+                  <b>Owner:</b> ${userData.first_name} ${userData.last_name}
+                </div>
+                <div class="description">
+            ${place.description}
+                </div>
+            </article>`;
+        $('.places').append(html);
+        });
+      }
+    }
+  });
+}
+
+function filterPlaces (url, amenitiesList) {
+  $.ajax({
+    url: url,
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({}),
+    success: function(data) {
+      for (const place of data) {
+        let matches = false;
+        for (let i = 0; i < amenitiesList.length; i++) {
+          if (place.) {}
+        }
+        $.get('http://127.0.0.1:5001/api/v1/users/' + place.user_id + '/' + amenitiesList, function (userData) {
           let guestsPlural = '';
           if (place.max_guest != 1) {
             let guestsPlural ='s';
