0c52461b958d28116aad11021d431d80bfb96908
YanielRamos <yanielramos0504@gmail.com>
2023-09-28 22:20:56 -0400

    file 3 uploading

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,58 @@
+// declares a constant variable $
+const $ = window.$;
+// sets up a function that will be executed when the HTML document has finished loading
+$(document).ready(function () {
+  const amenities = {};
+  // This event will be triggered whenever the user checks or unchecks a checkbox
+  $('INPUT[type="checkbox"]').change(function () {
+    // is checked, the code adds a new property to the amenities
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      // checkbox is not checked, code removes the corresponding property
+      delete amenities[$(this).attr('data-id')];
+    }
+    // selects H4 inside an element with class amenities and sets its text to the values
+    // of the properties on the amenities object, joined by a comma and a space.
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+
+    // Request status
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  // Request with
+  $.ajax({
+    type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: '{}',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: function(data) {
+      for (let i = 0; i < data.length; i++) {
+        $('section.places').append(`
+          <article>
+	  <div class="title_box">
+	    <h2>${data[i].name}</h2>
+	    <div class="price_by_night">$${data[i].price_by_night}</div>
+	  </div>
+	  <div class="information">
+	    <div class="max_guest">${data[i].max_guest}</div>
+            <div class="number_rooms">${data[i].number_rooms}</div>
+            <div class="number_bathrooms">${data[i].number_bathrooms}</div>
+	  </div>
+	  <div class="user">
+            <b>Owner:</b>${data[i].user.first_name} ${data[i].user.last_name}
+          </div>
+          <div class="description">
+	    ${data[i].description}
+          </div>
+	</article>
+        `)};
+      }
+      });
+});
