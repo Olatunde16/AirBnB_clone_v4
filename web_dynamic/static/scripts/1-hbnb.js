091cee8371c84ca07f766e6c49d76ebd8989e0af
YanielRamos <yanielramos0504@gmail.com>
2023-09-28 21:53:59 -0400

    updating ths js file

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,34 +1,13 @@
-$(document).ready(function() {
-  // Initialize an empty array to store checked Amenity IDs
-  var checkedAmenities = [];
+$(document).ready(function () {
+  const nameAmenity = [];
 
-  // Function to update the <h4> tag with the list of checked amenities
-  function updateAmenitiesList() {
-    var amenitiesList = checkedAmenities.join(', ');
-    $('.amenities h4').text('Selected Amenities: ' + amenitiesList);
-  }
-
-  // Listen for changes on each input checkbox
-  $('input[type="checkbox"]').on('change', function() {
-    var checkbox = $(this);
-    var amenityId = checkbox.data('id');
-
-    // Check if the checkbox is checked
-    if (checkbox.is(':checked')) {
-      // Add the Amenity ID to the array if it's not already there
-      if (checkedAmenities.indexOf(amenityId) === -1) {
-        checkedAmenities.push(amenityId);
-      }
+  $('input:checkbox').click(function () {
+    if ($(this).is(":checked")) {
+      nameAmenity.push($(this).attr('data-name'));
     } else {
-      // Remove the Amenity ID from the array if it's already there
-      var index = checkedAmenities.indexOf(amenityId);
-      if (index !== -1) {
-        checkedAmenities.splice(index, 1);
-      }
+      const nameIndex = nameAmenity.indexOf($(this).attr('data-name'));
+      nameAmenity.splice(nameIndex, 1);
     }
-
-    // Update the <h4> tag with the list of checked amenities
-    updateAmenitiesList();
+    $('.amenities h4').text(nameAmenity.join(', '));
   });
 });
-  
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
-    // Initialize an empty array to store checked Amenity IDs
-    var checkedAmenities = [];
+$(document).ready(function () {
+    const nameAmenity = [];
   
-    // Function to update the <h4> tag with the list of checked amenities
-    function updateAmenitiesList() {
-      var amenitiesList = checkedAmenities.join(', ');
-      $('.amenities h4').text('Selected Amenities: ' + amenitiesList);
-    }
+    $('input:checkbox').click(function () {
+      if ($(this).is(":checked")) {
+        nameAmenity.push($(this).attr('data-name'));
+      } else {
+        const nameIndex = nameAmenity.indexOf($(this).attr('data-name'));
+        nameAmenity.splice(nameIndex, 1);
+      }
+      $('.amenities h4').text(nameAmenity.join(', '));
+    });
   
     // Function to update the API status
     function updateApiStatus() {
       // Send an HTTP GET request to the status API
-      $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+      $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
         if (data.status === 'OK') {
           // If the status is "OK," add the "available" class to the div#api_status
           $('#api_status').addClass('available');
@@ -24,28 +27,5 @@ $(document).ready(function() {
   
     // Initial update of the API status
     updateApiStatus();
-  
-    // Listen for changes on each input checkbox
-    $('input[type="checkbox"]').on('change', function() {
-      var checkbox = $(this);
-      var amenityId = checkbox.data('id');
-  
-      // Check if the checkbox is checked
-      if (checkbox.is(':checked')) {
-        // Add the Amenity ID to the array if it's not already there
-        if (checkedAmenities.indexOf(amenityId) === -1) {
-          checkedAmenities.push(amenityId);
-        }
-      } else {
-        // Remove the Amenity ID from the array if it's already there
-        var index = checkedAmenities.indexOf(amenityId);
-        if (index !== -1) {
-          checkedAmenities.splice(index, 1);
-        }
-      }
-  
-      // Update the <h4> tag with the list of checked amenities
-      updateAmenitiesList();
-    });
-  });
+  });  
   
