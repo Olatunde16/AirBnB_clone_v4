13361c74592f39c831c53327e7e2cd6df946809b
agun36 <aguntoroti10@gamail.com>
2023-10-06 04:41:47 -0700

    akindele

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -4,8 +4,10 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -17,9 +19,9 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filter():
-    """ HBNB filters """
+@app.route('/101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010100-hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -30,9 +32,13 @@ def hbnb_filter():
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
 
-    return render_template('10-hbnb_filters.html',
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('100-hbnb.html',
                            states=st_ct,
-                           amenities=amenities)
+                           amenities=amenities,
+                           places=places, cache_id=uuid.uuid4)
 
 
 if __name__ == "__main__":
