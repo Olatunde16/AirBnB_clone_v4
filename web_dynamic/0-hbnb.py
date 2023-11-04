6fa3f0ca49c0a2d17db123734d7dcfa5e8fdbeca
H-Asmaa <asmaehadar32@gmail.com>
2023-11-04 19:43:05 +0100

    Committing a set up of the id in the <link> tags.

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,8 +7,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
-import uuid 
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -35,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
     cache_id = str(uuid.uuid4())
-    
+
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
@@ -45,4 +45,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
