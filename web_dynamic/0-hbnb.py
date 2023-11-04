163ea8da10cd32cdaa947123292bf2b797e96366
OUALIID <walidelhadim8@gmail.com>
2023-11-04 19:16:24 +0100

    update 2 files 0-hbnb.py and 0-hbnb.html

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,6 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
+import uuid 
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -33,13 +34,15 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-
-    return render_template('100-hbnb.html',
+    cache_id = str(uuid.uuid4())
+    
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
