cba6577660341b53b24fe706c853176dd092a783
elfadili-ae <elfadili.ae@gmail.com>
2023-11-04 09:24:36 +0000

    Fix: task 3

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,35 +33,37 @@ $(document).ready(function () {
         data: "{}",
         contentType: "application/json",
         dataType: "json",
-        success: function (data) {
-            $("SECTION.places").append(data.map(place => {
-                return `
-                    <article>
-	  <h2>${place.name}</h2>
-	  <div class="price_by_night">
-	    <p>${place.price_by_night}</p>
-	  </div>
-	  <div class="information">
-	    <div class="max_guest">
-	      <div class="guest_image"></div>
-	      <p>${place.max_guest}</p>
-	    </div>
-	    <div class="number_rooms">
-	      <div class="bed_image"></div>
-	      <p>${place.number_rooms}</p>
-	    </div>
-	    <div class="number_bathrooms">
-	      <div class="bath_image"></div>
-	      <p>${place.number_bathrooms}</p>
-	    </div>
-	  </div>
-	  <div class="user">
-	  </div>
-	  <div class="description">
-	    <p>${place.description}</p>
-	  </div>
-	</article>`
-            }))
-        }
+        success: fillPlaces,
     });
 });
+
+function fillPlaces(data) {
+    $("SECTION.places").append(data.map(place => {
+        return `
+            <article>
+<h2>${place.name}</h2>
+<div class="price_by_night">
+<p>${place.price_by_night}</p>
+</div>
+<div class="information">
+<div class="max_guest">
+  <div class="guest_image"></div>
+  <p>${place.max_guest}</p>
+</div>
+<div class="number_rooms">
+  <div class="bed_image"></div>
+  <p>${place.number_rooms}</p>
+</div>
+<div class="number_bathrooms">
+  <div class="bath_image"></div>
+  <p>${place.number_bathrooms}</p>
+</div>
+</div>
+<div class="user">
+</div>
+<div class="description">
+<p>${place.description}</p>
+</div>
+</article>`
+    }))
+};
