a33d8366db128025aef35b5185de90637c4991d5
pytbyte <heretolearn1@gmail.com>
2023-11-05 18:37:07 +0300

    v4 advanced update semi standard

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,34 +1,34 @@
 document.ready(function () {
-	const amenities = {};
-	$("li input[type=checkbox]").change(function () {
-		if (this.checked) {
-			amenities[this.dataset.name] = this.dataset.id;
-		} else {
-			delete amenities[this.dataset.name];
-		}
-		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
-	});
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete amenities[this.dataset.name];
+    }
+    $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
+  });
 
-	//API status
-	$.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
-		if (data.status === "OK") {
-			$("div#api_status").addClass("available");
-		} else {
-			$("div#api_status").removeClass("available");
-		}
-	});
+  // API status
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 
-	//places data
-	
-	$.post({
-		url: `${HOST}/api/v1/places_search`,
-		data: JSON.stringify({}),
-		headers: {
-			"Content-Type": "application/json",
-		},
-		success: (data) => {
-			data.forEach((place) =>
-				$("section.places").append(
+  // places data
+
+  $.post({
+    url: `${HOST}/api/v1/places_search`,
+    data: JSON.stringify({}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    success: (data) => {
+      data.forEach((place) =>
+        $('section.places').append(
 					`<article>
 			<div class="title_box">
 			<h2>${place.name}</h2>
@@ -36,22 +36,22 @@ document.ready(function () {
 			</div>
 			<div class="information">
 			<div class="max_guest">${place.max_guest} Guest${
-						place.max_guest !== 1 ? "s" : ""
+						place.max_guest !== 1 ? 's' : ''
 					}</div>
 			<div class="number_rooms">${place.number_rooms} Bedroom${
-						place.number_rooms !== 1 ? "s" : ""
+						place.number_rooms !== 1 ? 's' : ''
 					}</div>
 			<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-						place.number_bathrooms !== 1 ? "s" : ""
+						place.number_bathrooms !== 1 ? 's' : ''
 					}</div>
 			</div> 
 			<div class="description">
 			${place.description}
 			</div>
 				</article>`
-				)
-			);
-		},
-		dataType: "json",
-	});
+        )
+      );
+    },
+    dataType: 'json'
+  });
 });
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,33 +1,33 @@
 document.ready(function () {
-	const amenities = {};
-	$("li input[type=checkbox]").change(function () {
-		if (this.checked) {
-			amenities[this.dataset.name] = this.dataset.id;
-		} else {
-			delete amenities[this.dataset.name];
-		}
-		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
-	});
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete amenities[this.dataset.name];
+    }
+    $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
+  });
 
-	// API status
-	$.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
-		if (data.status === "OK") {
-			$("div#api_status").addClass("available");
-		} else {
-			$("div#api_status").removeClass("available");
-		}
-	});
+  // API status
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 
-	//  places data
-	$.post({
-		url: `${HOST}/api/v1/places_search`,
-		data: JSON.stringify({}),
-		headers: {
-			"Content-Type": "application/json",
-		},
-		success: (data) => {
-			data.forEach((place) =>
-				$("section.places").append(
+  //  places data
+  $.post({
+    url: `${HOST}/api/v1/places_search`,
+    data: JSON.stringify({}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    success: (data) => {
+      data.forEach((place) =>
+        $('section.places').append(
 					`<article>
 			<div class="title_box">
 			<h2>${place.name}</h2>
@@ -35,26 +35,26 @@ document.ready(function () {
 			</div>
 			<div class="information">
 			<div class="max_guest">${place.max_guest} Guest${
-						place.max_guest !== 1 ? "s" : ""
+						place.max_guest !== 1 ? 's' : ''
 					}</div>
 			<div class="number_rooms">${place.number_rooms} Bedroom${
-						place.number_rooms !== 1 ? "s" : ""
+						place.number_rooms !== 1 ? 's' : ''
 					}</div>
 			<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-						place.number_bathrooms !== 1 ? "s" : ""
+						place.number_bathrooms !== 1 ? 's' : ''
 					}</div>
 			</div> 
 			<div class="description">
 			${place.description}
 			</div>
 				</article>`
-				)
-			);
-		},
-		dataType: "json",
-	});
+        )
+      );
+    },
+    dataType: 'json'
+  });
 
-	// search places
-	$(".filters button").bind("click", searchPlace);
-	searchPlace();
+  // search places
+  $('.filters button').bind('click', searchPlace);
+  searchPlace();
 });
