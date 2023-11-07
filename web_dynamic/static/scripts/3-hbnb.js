3cd9538bf9f7e9f9b9f5a22fa46f63b3d8e2a173
mohamed-622 <mohamedezgoudi@gmail.com>
2023-11-07 01:12:02 +0100

    task_4 to be merged with master

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  const amenities = {};
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[this.dataset.name] = this.dataset.id;
+      $('.amenities h4').text(Object.keys(amenities).join(', '));
+    } else {
+      delete amenities[this.dataset.name];
+    }
+    if (Object.keys(amenities).length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
+    }
+  });
+  $.post({
+    URL: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: JSON.stringify({ }),
+    dataType: 'json',
+    Headers: { 'Content-Type': 'application/json' },
+    success: function (data) {
+      for (const place of data) {
+        $('.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>');
+      }
+    }
+  });
+});
