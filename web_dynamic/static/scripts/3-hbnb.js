2142076f68d1ef215f27e44874f78a877f9dbf8b
mohamed-622 <mohamedezgoudi@gmail.com>
2023-11-07 01:33:42 +0100

    getJason deleted by mistake

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -13,6 +13,15 @@ $(document).ready(function () {
       $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
     }
   });
+  
+  $.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
+		if (data.status === "OK") {
+			$("div#api_status").addClass("available");
+		} else {
+			$("div#api_status").removeClass("available");
+		}
+	});
+
   $.post({
     URL: 'http://0.0.0.0:5001/api/v1/places_search/',
     data: JSON.stringify({ }),
