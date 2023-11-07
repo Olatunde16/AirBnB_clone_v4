0dff862417d77ef1acc62407127425b86e53d4e8
patrice <patricedev02.com>
2023-11-07 00:47:27 +0000

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,12 @@
-window.addEventListener('load', function () {
-  // task 2
-  const amenityIds = {};
-  $('input[type=checkbox]').change(function () {
-    if ($(this).prop('checked')) {
-      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else if (!$(this).prop('checked')) {
-      delete amenityIds[$(this).attr('data-id')];
-    }
-    if (Object.keys(amenityIds).length === 0) {
-      $('div.amenities h4').html('&nbsp');
-    } else {
-      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
-    }
+$(document).ready(function () {
+	const checkedAmenity = [];
+	$('input:checkbox').click(function () {
+	  if ($(this).is(":checked")) {
+		checkedAmenity.push($(this).attr('data-name'));
+	  } else {
+		const nameIndex = checkedAmenity.indexOf($(this).attr('data-name'));
+		checkedAmenity.splice(nameIndex, 1);
+	  }
+	  $('.amenities h4').text(checkedAmenity.join(', '));
+	});
   });
-});
