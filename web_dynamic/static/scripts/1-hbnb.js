f3f876fe7fb486115c64ee1b9003a98b966eeea0
matternstsr <matternstsr@gmail.com>
2023-12-07 16:43:54 -0600

    Updated 0-hbnb to 1-hbnb -  Changed how accesses the file other wise it might access the cached one.   /  Changed the checkbox input for funtionality and calling (syntax)   ----   Finished out the 1-hbnb.js with a way to update the amenities using the map methode to create a new array

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,19 @@
-function DocumentReady () {
+function DocumentReady() {
   const amenityList = [];
   $('li input:checkbox').change(function () {
-    const amenityObj = {id: this.attribute('data-id'), name: this.attribute('data-name')};
-    if (this.checked) {
-      amenityList.push(amenityObj);
-    } else {
-      amenityList.pop(amenityObj)
-    }
+      const amenityObj = { id: $(this).data('id'), name: $(this).data('name') };
+      if (this.checked) {
+          amenityList.push(amenityObj);
+      } else {
+          amenityList = amenityList.filter(item => item.id !== amenityObj.id);
+      }
+      updateSelectedAmenities(amenityList);
   });
+}
+
+function updateSelectedAmenities(amenityList) {
+  const selectedAmenitiesList = amenityList.map(item => item.name).join(', ');
+  $('#selected-amenities').text(selectedAmenitiesList);
 }
 
 $(document).ready(DocumentReady);
