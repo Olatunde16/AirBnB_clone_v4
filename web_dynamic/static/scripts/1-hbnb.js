e4f5b7ee7f41596e1bd586ab9d2902a68688ae1b
Allison <allisonabinger@gmail.com>
2023-12-07 16:56:39 -0600

    A - 12/07 04:56 PM - task 2 - checkbox is FULLY functional and tested

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,12 @@
-$()
+$(document).ready(function () {
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    const amenityNames = Object.values(amenities);
+    $('.amenities h4').text(amenityNames.join(', '));
+  });
+});
