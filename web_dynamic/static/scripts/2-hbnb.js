6a125b1b9ecc8168a8c1941be325236edc10be91
Travis Adamson <tndadamson@gmail.com>
2023-12-07 22:06:31 -0600

    Rebuilding repository from fatal merge conflict

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$('document').ready(function () {
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/status/',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });
+
+  let amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    if (Object.values(amenities).length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    }
+  });
+});
