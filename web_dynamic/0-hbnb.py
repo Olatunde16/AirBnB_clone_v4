a6803ac0a8e4a5fdae425cb30c417b450c8e4d97
MarkMusili <musiliyrn@gmail.com>
2024-01-13 15:19:49 +0300

    Path to html template update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -21,7 +21,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -37,7 +37,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            cache_id=cache_id,
