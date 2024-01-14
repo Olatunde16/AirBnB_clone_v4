0d793d659c41bdec72511a84f7ef52e7123687d1
MarkMusili <musiliyrn@gmail.com>
2024-01-14 13:47:57 +0300

    Java script update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,12 @@
 $(function () {
     const amenityDict = {};
 
-    $(input[type="checkbox"]).change(function () {
-        if (this.checked) {
-            amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    $('input').click(function () {
+        if ($(this).is(':checked')) {
+            amenityDict[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenityDict[$(this).data('id')];
         }
-        else {
-            delete amenityDict[$(this).attr('data-id')];
-        }
-        let amenityList = Object.values(amenityDict);
-        if (amenityList.length > 0) {
-            $('div.amenities  h4').text(Object.values(amenityDict).join(', '));
-        }
-        else {
-            $('div.amenities  h4').html('&nbsp;');
-        }
+        $('.amenities h4').text(Object.values(amenityDict).join(', '));
     });
 })
