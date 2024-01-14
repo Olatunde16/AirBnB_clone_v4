d919f8508d2ffbefea171beb8bc03973b91a51b3
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-01-14 17:08:04 +0000

    API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,3 +15,16 @@ $(function () {
     console.log(myId);
   });
 });
+$.ajax({
+  url: 'http://0.0.0.0:5001/api/v1/status/',
+  type: 'GET',
+  dataType: 'json',
+  success: function (json) {
+    $('#api_status').addClass('available');
+  },
+
+  error: function (xhr, status) {
+    console.log('error ' + status);
+  }
+
+});
