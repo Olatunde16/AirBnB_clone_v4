b6f1256821f36ce8428fd825a1cb9c26611b2606
MarkMusili <musiliyrn@gmail.com>
2024-01-14 16:27:49 +0300

    Mark's branch

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+$(function () {
+    const amenityDict = {};
+
+    $('input').click(function () {
+        if ($(this).is(':checked')) {
+            amenityDict[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenityDict[$(this).data('id')];
+        }
+        $('.amenities h4').text(Object.values(amenityDict).join(', '));
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+            $('DIV#api_status').addClass('available');
+        } else {
+            $('DIV#api_status').removeClass('available');
+        }
+    }).fail(function (xhr, textStatus, error) {
+        console.log('Error: ' + error);
+    });
+});
