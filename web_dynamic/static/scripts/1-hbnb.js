5108a985b7771100ccde732534201c48c29bbb43
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-02 23:06:27 +0200

    fixing errors

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,24 @@
-$(document).ready(function() {
-    $('#data-id').change(function() {
-        if ($(this).is(':checked')) {
-        let data_id = amenity.id;
-        } else {
-            data_id = "";
-        }
-    })
+$(document).ready(function () {
+	console.log('Document loaded successfully!');
+
+	const amenityStorage = {};
+
+	$('li :checkbox').change(function () {
+		let amenityId = $(this).attr('data-id');
+		let amenityName = $(this).attr('data-name');
+
+		if(this.checked){
+			amenityStorage[amenityId] = amenityName;
+		} else {
+			delete amentityStorage[amenityId]
+		}
+
+		$('div.amenities h4').empty();
+
+		let newText =  $.map(amenityStorage, function (v) {
+			return v;
+		}).join(', ');
+
+		$('div,amenities h4').text(newText);
+	});
 });
