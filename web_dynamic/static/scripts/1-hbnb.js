af89d74a3862a57c15a010a2d103769f059d6245
chimaskyy <chiamakahj@gmail.com>
2024-03-02 06:03:48 +0100

    not completed

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,10 @@
+// script that executes when the DOM is ready
+// and adds event listeners on each input checkbox
+
+
+$(document).ready(function () {
+    let amenity_dict = {}
+    $("input:checkbox").change(function () {
+        amenity_dict["id"] = data_id
+    });
+});
