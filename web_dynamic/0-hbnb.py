45287f67a4596dc30912dd4132d4985430d42537
lukeschula <schulaluke@gmail.com>
2024-04-04 11:56:27 -0500

    added cahe id variable

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -24,6 +24,7 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
+    cache_id = uuid.uuid4()
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -37,7 +38,7 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places, cache_id = cache_id)
 
 
 if __name__ == "__main__":
