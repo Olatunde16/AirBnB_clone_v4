58df875dcc7e29b0a954a03d8228515e3679e60d
Sibongile <sibongile.nhlema@gmail.com>
2024-04-04 21:49:34 +0200

    Implement POST request for places searc

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -12,7 +12,16 @@ $(document).ready(function () {
     $('.amenities h4').text(amenitiesList);
   });
 
-  $
+  $.post('http://0.0.0.0:5001/api/v1/places_search/', JSON.stringify({}), (data) => {
+    const sectionPlaces = $('section.places');
+
+    data.forEach(place => {
+      const article = `<article><div class="title_box"><h2>${place.name}</h2><div class="price_by_night">$${place.price_by_night}</div></div><div class="information"><div class="max_guest">${place.max_guest} Guests</div><div class="number_rooms">${place.number_rooms} Bedrooms</div><div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div></div><div class="description">${place.description}</div></article>`;
+      sectionPlaces.append(article);
+    });
+  }).fail((error) => {
+    console.error('Error fetching places:', error);
+  });
 
   $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
     const apiStatus = $('div#api_status');
