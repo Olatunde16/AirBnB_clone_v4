d84974355e7335f791d89c5bfd7752cd6017d984
Sibongile <sibongile.nhlema@gmail.com>
2024-04-04 17:18:07 +0200

    redoing js file

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $('.amenities input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+
+    const amenitiesList = Object.values(amenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+
+  $
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    const apiStatus = $('div#api_status');
+
+    if (data.status === 'OK') {
+      apiStatus.addClass('available');
+    } else {
+      apiStatus.removeClass('available');
+    }
+  });
+});
