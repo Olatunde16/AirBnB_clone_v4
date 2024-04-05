c4e120e675caf7f943224414f9bd0cf69db34e26
Owen Gitau <owengitau02@gmail.com>
2024-04-05 21:31:22 +0300

    Changes

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+from flask import Flask, render_template, url_for
 import uuid
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from os import environ
-from flask import Flask, render_template
 
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -23,7 +18,7 @@ def teardown_db(exeption):
     """ Remove the current SQLAlchemy Session  after each request """
     storage.close()
 
-@app.route('/1-hbnb/')
+@app.route('/1-hbnb')
 def filters_hbnb(the_id=None):
     """ for custom filter requests for states, cities and amenities modules"""
     state_objects = storage.all('State').values()
@@ -31,7 +26,8 @@ def filters_hbnb(the_id=None):
     amenities = storage.all('Amenity').values()
     places =storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-    for user in storage.all('User').values())
+                 for user in storage.all('User').values())
+
     return render_template('1-hbnb.html'
                             states=states,
                             amenities=amenities,
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,16 @@
-$(document).ready(function () {
-    let amenitiesChecked = []
-    $('input ["type=checkbox"]').change(function() {
-      if (this.checked) {
-        let amenityId = $(this).attr('data-id');
-        amenitiesChecked.push(amenityId);
-       }
-      else {
-        amenitiesChecked.splice($.inArray(checked, amenitiesChecked), 1);
-      }
-      $('.amenities h4').text(amenitiesChecked);
-    });
+window.addEventListener('load', function () {
+  // task 2
+  const amenityIds = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
   });
+});
