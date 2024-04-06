57b88b30d535d143f450af1cd0f2b0d3d9e1a505
ally2211 <amendieta.va@gmail.com>
2024-04-05 21:20:21 -0500

    pycode

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,9 +7,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+import uuid
 
 app = Flask(__name__)
-import uuid
+
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -45,5 +46,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000
-            )
+    app.run(host='0.0.0.0', port=5000)
