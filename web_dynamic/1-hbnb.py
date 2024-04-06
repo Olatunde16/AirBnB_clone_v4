07148d710fb82aca49a463aaf6a2541dccbada4f
oumaima <oumaimanaanaapro@gmail.com>
2024-04-06 16:42:27 +0000

    task 2

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -36,7 +36,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
     cache_id = str(uuid.uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places, cache_id=cache_id)
---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	const checkedAmenities = {};
+
+	$('input[type="checkbox"]').change(function() {
+		const amenityId = $(this).attr('data-id');
+		const amenityName = $(this).attr('data-name');
+
+		if ($(this).prop('checked')) {
+			checkedAmenities[amenityId] = amenityName;
+		} else {
+			delete checkedAmenities[amenityId];
+		}
+	const amenitiesList = Object.values(checkedAmenities).join(', '));
+	$('.amenities h4').text(amenitiesList);
+	$('#checkedAmenitiesList').text("Checked amenities:" + amenitiesList);
+
+	});
+});
