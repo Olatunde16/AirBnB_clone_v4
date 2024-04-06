26c9230068702c4ebb38c69a8436af8b6988cb9b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-04-06 14:21:51 -0500

    retest for import error

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-from ..models import storage
-from ..models.state import State
-from ..models.city import City
-from ..models.amenity import Amenity
-from ..models.place import Place
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
