36f24a01a2b36e625809ede4d02d1450299f04dd
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-04-07 09:30:39 +0300

    added 1-hbnb code

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+  $(document).ready(function() {
+    // Creating an empty dictionary to hold amenities with their status
+    let amenities = {};
+
+    // Listening for changes on checkboxes
+    $("input[type=checkbox]").change(function() {
+        let amenityId = $(this).data('data-id');
+        if ($(this).is(':checked')) {
+            amenities[amenityId] = true;
+        } else {
+            delete amenities[amenityId];
+        }
+
+        // Update the list of amenities in the h4 tag
+        let amenitiesList = Object.keys(amenities).join(", ");
+        $('.amenities h4').text(amenitiesList);
+    }); 
+
     let url = "http://0.0.0.0:5001/api/v1/status/";
     $.get(url, function(data) {
         if (data.status === "OK") {
@@ -8,4 +26,3 @@ $(document).ready(function() {
         }
     });
 });
-
