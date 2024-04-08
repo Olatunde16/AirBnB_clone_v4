061d2a74f8d233d1e431efb59bcd35fa12869b3b
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-04-08 12:45:36 -0500

    Finishing Up Task 2?

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  let amenitiesChecked = {};
+
+  $('input[type="checkbox"]').change(function() {
+    if (this.checked) {
+      amenitiesChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesChecked[$(this).data('id')];
+    }
+    // Join the names of the checked amenities and update the h4 tag
+    $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
+  });
+
+//Get API Status
+$.get('http://localhost:5001/api/v1/status/', (response) => {
+  if (response.status === "OK") {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+}).fail(() => {
+  $('div#api_status').removeClass('available');
+});
+});
