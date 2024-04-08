8023b5314240be51c00267c32f5a35253d7a0209
ally2211 <amendieta.va@gmail.com>
2024-04-08 11:51:26 -0500

    status and add text for checklist

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
       // This function executes when the state of a checkbox changes
       // Get the Amenity ID from the data-id attribute of the checkbox's parent <li> tag
       var amenityID = $(this).closest('li').data('id');
+      var amenityName = $(this).closest('li').data('name');
       // Check if the checkbox is checked
       if ($(this).is(':checked')) {
           // If checked, add the Amenity ID to the checkedAmenities object
-          checkedAmenities[amenityID] = true;
+          checkedAmenities[amenityID] = amenityName;
       } else {
           // If unchecked, remove the Amenity ID from the checkedAmenities object
           delete checkedAmenities[amenityID];
@@ -20,13 +21,8 @@ $(document).ready(function() {
   });
   // Function to update the h4 tag inside the div with id "amenities"
   function updateAmenitiesList() {
-      // Get the keys (Amenity IDs) from the checkedAmenities object
-      var amenityIDs = Object.keys(checkedAmenities);
-      // Get the names of amenities corresponding to the checked Amenity IDs
-      var amenityNames = amenityIDs.map(function(id) {
-          return $('li[data-id="' + id + '"]').data('name');
-      });
-      // Update the text inside the h4 tag with the list of checked amenities separated by commas
-      $('div#amenities > h4').text(amenityNames.join(', '));
+      var amenityNames = Object.values(checkedAmenities);
+      // Update the content of #checkedAmenitiesList with the names of checked amenities
+      $('#checkedAmenitiesList').text(amenityNames.join(', '));
   }
 });
