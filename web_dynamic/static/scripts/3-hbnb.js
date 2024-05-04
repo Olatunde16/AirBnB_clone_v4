d98cfbaab59dbaed111724746cd0f27d77e01a14
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-05-04 22:51:37 +0100

    Fetch places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -30,23 +30,28 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
 
 $(document).ready(function() {
   $.post({
-    url:('http://0.0.0.0:5001/api/v1/places_search/?format=json', function (data) {
-  for (const place of data) {
-    const article = $('<article></article>');
+    url:'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      for (const place of data) {
+        const article = $('<article></article>');
 
-    const titleBox = $('<div class="title_box"></div>');
-    titleBox.append($('<h2></h2>').text(place.name));
-    titleBox.append($('<div class="price_by_night"></div>').text('$' + place.price_by_night));
-    article.append(titleBox);
+        const titleBox = $('<div class="title_box"></div>');
+        titleBox.append($('<h2></h2>').text(place.name));
+        titleBox.append($('<div class="price_by_night"></div>').text('$' + place.price_by_night));
+        article.append(titleBox);
 
-    const information = $('<div class="information"></div>');
-    information.append($('<div class="max_guest"></div>').text(place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '')));
-    information.append($('<div class="number_rooms"></div>').text(place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '')));
-    information.append($('<div class="number_bathrooms"></div>').text(place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '')));
-    article.append(information);
+        const information = $('<div class="information"></div>');
+        information.append($('<div class="max_guest"></div>').text(place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '')));
+        information.append($('<div class="number_rooms"></div>').text(place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '')));
+        information.append($('<div class="number_bathrooms"></div>').text(place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '')));
+        article.append(information);
 
-    article.append($('<div class="description"></div>').text(place.description));
+        article.append($('<div class="description"></div>').text(place.description));
 
-    $('.places').append(article);
-  }
+        $('.places').append(article);
+      }
+    },
+  });
 });
